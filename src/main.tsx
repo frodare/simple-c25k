9eb6581bbb3b758d92800354b414c1397cb53c8c
Charles Howard <frodare@gmail.com>
2023-11-19 09:33:11 -0500

    getting started

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+
+/*
+- get the PWA working
+- figure how to deploy to github pages
+- data structure to hold schedule
+- redux store (play with the new redux stuff)
+- create github repo to save files
+- excalidraw to draw out the UI, save locally
+
+
+
+
+
+
+*/
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
