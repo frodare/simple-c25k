d822123c15ce77f39b9c3d0bebe919f04d8c36fb
Charles Howard <frodare@gmail.com>
2023-11-22 07:54:05 -0500

    adding TODO notes

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import { Provider } from 'react-redux'
 import store from './store/index.ts'
 
 /*
+- BUG: when paused, navigating to first interval and starting puts you at a negative time
+- BUG: going to prev interval, when it will be the first, should reset to 0, or there should be a reset?
+- center bg image
+- add install button
 - trigger an alarm sound / vibration?
 - add a background image
 - keep screen active
